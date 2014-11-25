6ba330ab5d9a095b893e35a0a4caaa5f7dc1b655
Gabor Csardi <csardi.gabor@gmail.com>
2014-11-24 23:52:45 -0500

    Proper, empty package

---
+++ R/corpora.R
@@ -0,0 +1,12 @@
+
+#' Load a data set from the corpora package
+#'
+#' @param which The data set to load, a string. If not given, then
+#'   all data sets in the package are listed.
+#' @return A data frame containing the data set.
+#'
+#' @export
+
+corpora <- function(which) {
+
+}
